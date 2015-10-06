79f327770a15e9b146ecb9653fc84d5c58736de9
Solinari <justin.tyme.dejesus@gmail.com>
2015-10-06 12:44:40 -0500

    encapsulated process in functions for readability
    Next step is to determine if degree requirements and options can be
    parsed from these set of links

--- Justin's Parser Tests/BS4Tests.py
+++ Justin's Parser Tests/BS4Tests.py
@@ -1,56 +1,83 @@
 from bs4 import BeautifulSoup
 import urllib.request
 
+# these will all be encapsulated in functions eventually
 
 url = "http://www.cdm.depaul.edu/academics/Pages/MastersDegrees.aspx"
-
 fronturl = "http://www.cdm.depaul.edu"
-degreeLinks = []
-degreesAndConcentrations = []
 
+# function 1 - get the degree links
+def findDegreeLinks(url):
+    '''finds degree links from  url passed on DePaul's page'''
+    myDegreeLinks = []
+    urlOpen = urllib.request.urlopen(url)
+    soup = BeautifulSoup(urlOpen, 'html.parser')
 
-urlOpen = urllib.request.urlopen(url)
-soup = BeautifulSoup(urlOpen, 'html.parser')
+    # get all the program url's and append them 
+    test1 = soup.find_all("p", "programIndexHeader")
+    #print(len(test1))
 
-# get all the program url's and append them 
-test1 = soup.find_all("p", "programIndexHeader")
-#print(len(test1))
+    for i in test1:
+        myDegreeLinks.append(str(i).split(' ')[3].split('\"')[1])
 
-for i in test1:
-    degreeLinks.append(str(i).split(' ')[3].split('\"')[1])
+    return myDegreeLinks
 
-#print(degreeLinks)
-Concentrations = []
-for degree in degreeLinks:
-    concentrationLink = fronturl + degree
-    #print(concentrationLink)
-    degreesAndConcentrations.append(concentrationLink)
+# end function 1
 
-    # find if concentratinos are present
-    urlOpen2 = urllib.request.urlopen(concentrationLink)
-
-    soup2 = BeautifulSoup(urlOpen2, 'html.parser')
-
-    test2 = soup2.find_all("h2", "moduletitle")
-
-    for i in test2:
-        sample = str(i).split("\"")
-        #print(sample)
-
+#function 2 - map concentration links
+def findDegreeConcentrations(myDegreeLinks, myFrontUrl):
+    '''Find concentrations in degrees'''
+    myDegreesAndConcentrations = []
+    for degree in myDegreeLinks:
+        concentrationLink = myFrontUrl + degree
+        #print(concentrationLink)
+        outputSize = len(myDegreesAndConcentrations)
         
 
-        for element in sample:
+        # find if concentrations are present
+        # append them if found
+        urlOpen2 = urllib.request.urlopen(concentrationLink)
+
+        soup2 = BeautifulSoup(urlOpen2, 'html.parser')
+
+        test2 = soup2.find_all("h2", "moduletitle")
+
+        for i in test2:
+            sample = str(i).split("\"")
+            #print(sample)
+
             
-             if "/academics/Pages" in element:
-                 #print(element)
-                 if fronturl not in element:
-                     element = fronturl + element
-                     
-                 degreesAndConcentrations.append(element)
+
+            for element in sample:
+                
+                 if "/academics/Pages" in element:
+                     #print(element)
+                     if fronturl not in element:
+                         element = fronturl + element
+                         
+                     myDegreesAndConcentrations.append(element)
+
+        # if you didn't find anything in the concentrations
+        # append that degree url
+        if outputSize == len(myDegreesAndConcentrations):
+            myDegreesAndConcentrations.append(concentrationLink)
 
 
-#all degrees with their concentrations
-print(len(degreesAndConcentrations))
-for degree in degreesAndConcentrations:
-    print(degree)
+    #all degrees with their concentrations
+    print(len(myDegreesAndConcentrations))
+    for degree in myDegreesAndConcentrations:
+        print(degree)
+
+# end function 2
+degreeLinks = findDegreeLinks(url)
+degreesAndConcentrations = findDegreeConcentrations(degreeLinks, fronturl)
+
+
+
+
+
+
+
+
+
 
