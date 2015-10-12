2c7a60f7dd49c269fb6029edd5399f6abc286c74
Kevin Rodriguez <kevin.rodriguez@me.com>
2015-10-12 12:05:37 -0500

    Url to grab course info
    Program that grabs the required class for a degree and then puts them
    as webpage to grab the course info and when classes are offered

--- Python/Catalog.py
+++ Python/Catalog.py
@@ -1,9 +1,16 @@
-import unicodedata
-import math
-import urllib2
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
+import urllib.request
 
-file = open("animation-ta.txt", "w")
-page = urllib2.urlopen("http://www.cdm.depaul.edu/academics/Pages/CourseCatalog.aspx")
-soup = BeautifulSoup(page)
-print soup
+file = open("BIT.txt", "r") #file of  class required for degree. Can change to whatever we need
+line = file.readline()
+str = line.replace('|',' ')
+list1 = str.split(' ')
+print (list1)
+i = 0
+while i < len(list1):
+  subject = list1[i]
+  cNum = list1[i + 1]
+  url = "http://www.cdm.depaul.edu/academics/pages/courseinfo.aspx?Subject=" + subject + "&CatalogNbr=" + cNum
+  print(url)
+  # need to save in a list
+  i = i + 2
