e0a8d114e2e944adf2b8999f0e110eef3270b883
Justin Tyme Dejesus <justin.tyme.dejesus@gmail.com>
2015-11-07 18:01:08 -0600

    parser to csv is working but not capturing course id major and professor

--- Parser Into CSV/ParserDump2CSV.py
+++ Parser Into CSV/ParserDump2CSV.py
@@ -12,9 +12,6 @@ import urllib.request
 def runParse():
         '''run the fucking god damn parser'''
         parsedData =[]
-
-
-
         ##########################
         classList = []
         # list = ['animation-animator.txt','animation-ta.txt','BIT.txt','cinema-post.txt','cinema-production.txt',
@@ -39,7 +36,7 @@ def runParse():
                         classList.append(url)
                 i = i + 2
 
-        print (classList[0])
+        print(classList[0])
         urlOpen = urllib.request.urlopen(classList[0])
         soup = BeautifulSoup(urlOpen,'html.parser')
         header = soup.find("h2","CDMPageTitle")
@@ -60,23 +57,31 @@ def runParse():
                 for element in classText:
                         if "Section" in element:
                                 elementwodiv = element.replace("</div>","")
-                                newDataRow.append(elementwodiv)
-                                print(element.replace("</div>",""))
+                                elementworn = elementwodiv.replace("\r\n","")
+                                elementwospace = elementworn.replace("  ", "")
+                                newDataRow.append(elementwospace)
+                                print(elementwospace)
                                                         
                         if "Class number" in element:
                                 elementwodiv = element.replace("</div>","")
-                                newDataRow.append(elementwodiv)
-                                print(element.replace("</div>",""))
+                                nelementworn = elementwodiv.replace("\r\n","")
+                                elementwospace = elementworn.replace("  ", "")
+                                newDataRow.append(elementwospace)
+                                print(elementwospace)
                                 
                         if "Meeting time" in element:
                                 elementwodiv = element.replace("</div>","")
-                                newDataRow.append(elementwodiv)
-                                print(element.replace("</div>",""))
+                                elementworn = elementwodiv.replace("\r\n","")
+                                elementwospace = elementworn.replace("  ", "")
+                                newDataRow.append(elementwospace)
+                                print(elementwospace)
                                 
                         if "Location" in element:
                                 elementwodiv = element.replace("</div>","")
-                                newDataRow.append(elementwodiv)
-                                print(element.replace("</div>",""))
+                                elementworn = elementwodiv.replace("\r\n","")
+                                elementwospace = elementworn.replace("  ", "")
+                                newDataRow.append(elementwospace)
+                                print(elementwospace)
 
                 #this would be instructor when its found
                 #TODO - Instructor
@@ -92,70 +97,30 @@ def runParse():
         # for the below line:
         # parsedData.append(row)
         ###############
-
         return parsedData
 
+# this works
 
-def parsedDataToDataframe(parsedData):
-	'''dump all rows in data to dataframe'''
+def parsedDataToDataFrame(parsedData):
+        '''dumps all rows to data frame'''
 
-	#all fields should be strings***
+        #print(parsedData)
+        season = []
+        section = []
+        classNumber = []
+        meetingTime = []
+        location = []
+        instructor = []
 
-	# we have 7 data fields per row
+        for row in parsedData:
+                season.append(row[0])
+                section.append(row[1])
+                classNumber.append(row[2])
+                meetingTime.append(row[3])
+                location.append(row[4])
+                instructor.append(row[5])
+        print("{}\n{}\n{}\n{}\n{}\n{}".format(season, section, classNumber, meetingTime, location, instructor))
+        parsedDataDataFrame = pd.DataFrame({'Season':season,'Section':section,'Class Number':classNumber,'Meeting Time':meetingTime,'Location':location,'Instructor':instructor})
+        parsedDataDataFrame.to_csv('Catalog.csv',index=False,columns=['Major','Season','Section','Class Number','Meeting Time','Location','Instructor'], engine='python')
 
-	# append this either as a link url or a defined constant
-	major = []
-
-	# ie Winter 2015-2016
-	season = []
-
-	# course section
-	section = []
-
-	# Class ID Number
-	classNumber = []
-
-	# Meeting Time
-	meetingTime = []
-
-	# Room name/number & Campus
-	location = []
-
-	# Instructor - Issue parsing Instructor in parse just enter empter string for this row
-	instructor = []
-
-
-	# so the way this works, we are going to do a Matrix Inversion
-	# don't worry about the math, its just turning the table "on its side"
-	# for data storage reasons
-	# See how for each row I take an element and append it to these new fields?
-	for row in parsedData:
-                print("row is {} long".format(len(row)))
-                print(row)
-                major.append(row[0])
-                season.append(row[1])
-                section.append(row[2])
-                classNumber.append(row[3])
-                meetingTime.append(row[4])
-                location.append(row[5])
-                instructor.append(row[6])
-
-
-	# This is why it matters the order as well as 
-
-	parsedDataDataFrame = pd.DataFrame({'Major' : major,
-										'Season' : season,
-										'Section' : section,
-										'Class Number' : classNumber,
-										'Meeting Time' : meetingTime,
-										'Location' : location,
-										'Instructor' : instructor})
-
-	parsedDataDataFrame.to_csv('Catalog.csv',
-								index=False,
-								columns=['Major', 'Season', 'Section',
-								'Class Number', 'Meeting Time',
-								'Location', 'Instructor'],
-								engine='python')
-
-parsedDataToDataframe(runParse())
+parsedDataToDataFrame(runParse())
