e2f144e212cedcc826dc26637ccc445e37fb701d
Justin Tyme Dejesus <justin.tyme.dejesus@gmail.com>
2015-11-09 00:11:10 -0600

    issue with parser not saving some columns. data is present

--- Parser Into CSV/ParserDump2CSV.py
+++ Parser Into CSV/ParserDump2CSV.py
@@ -1,129 +1,193 @@
-#parserDataDump.py
+# parserDataDump.py
 
 # for pandas
-import math
 import pandas as pd
 
 # for parsing
 from bs4 import BeautifulSoup
 import urllib.request
 
-def deDivElement(e):
-        '''remove just </div><div> tags'''
 
-        out = e.replace("</div>","")
-        
-        return out
+
+def deDivElement(e):
+    '''remove just </div><div> tags'''
+
+    out = e.replace("</div>", "")
+    out = out.replace("<div>", "")
+
+    return out
+
 
 def deWhiteSpaceElement(e):
-        '''strip out the whitepsace in a parsed element'''
-        
-        out = "".join(line.strip() for line in e.split("\n"))
+    '''strip out the whitepsace in a parsed element'''
 
-        return out
-        
+    out = "".join(line.strip() for line in e.split("\n"))
+
+    return out
+
+def ampersandCleaner(e):
+    '''removes amp; from urls'''
+
+    out = e.replace("amp;","")
+
+    return out
+
 def runParse():
-        '''run parser'''
-        parsedData =[]
-        ##########################
-        classList = []
-        # list = ['animation-animator.txt','animation-ta.txt','BIT.txt','cinema-post.txt','cinema-production.txt',
-        #     'cinema-sound.txt','cns-governance.txt','cns-networkSecurity.txt','cns-security.txt','comp-finance.txt',
-        #     'comp-science.txt','e-commerce.txt','game-development.txt','hci.txt','health-informatics.txt',
-        #     'is-bi.txt','is-bs.txt','is-da.txt','is-enterprise.txt','is-standard.txt','it-project.txt',
-        #     'media-arts.txt','network-engineering.txt','pa-comp.txt','pa-healthcare.txt','pa-hospitality.txt',
-        #     'pa-marketing.txt','se-entrepreneurship.txt','se-game.txt','se-project.txt','se-software.txt','se-softwareDev.txt']
-        # for item in list:
-        file = open('BIT.txt', "r") #file of  class required for degree. Can change to whatever we need
+    '''run parser'''
+
+    depaul = "http://www.cdm.depaul.edu"
+    parsedData = []
+    allMajors = ['BIT.txt','animation-animator.txt','animation-ta.txt','BIT.txt','cinema-post.txt','cinema-production.txt',
+                 'cinema-sound.txt','cns-governance.txt','cns-networkSecurity.txt','cns-security.txt','comp-finance.txt',
+                 'comp-science.txt','e-commerce.txt','game-development.txt','hci.txt','health-informatics.txt',
+                 'is-bi.txt','is-bs.txt','is-da.txt','is-enterprise.txt','is-standard.txt','it-project.txt',
+                 'media-arts.txt','network-engineering.txt','pa-comp.txt','pa-healthcare.txt','pa-hospitality.txt',
+                 'pa-marketing.txt','se-entrepreneurship.txt','se-game.txt','se-project.txt','se-software.txt','se-softwareDev.txt']
+
+    found = set()
+    
+    for major in allMajors:
+        file = open(major, "r")
+        print("Opening: {}".format(major))
         line = file.readline()
-        string = line.replace('|',' ')
-        list1 = string.split(' ')
 
-        i = 0
-        while i < len(list1):
-                subject = list1[i]
-                cNum = list1[i + 1]
-                url = "http://www.cdm.depaul.edu/academics/pages/courseinfo.aspx?Subject=" + subject + "&CatalogNbr=" + cNum
-                test = url in classList
-                if test == False:
-                    #classList.append(url)
-                    newDataRow = []
-                    urlOpen = urllib.request.urlopen(url)
-                    soup = BeautifulSoup(urlOpen,'html.parser')
-                    header = soup.find("h2","CDMPageTitle")
-                    text = header.getText()
-                    newDataRow.append(text)
-                    divs = soup.find("div","schedule")
-                    classInfo = soup.find_all("div","classInfo")
-                    for info in classInfo:
-                        #for item in divs:
-                        term = soup.find("p","CTIPageSectionHeader")
-                        pText = term.getText()
-                        #print(deWhiteSpaceElement(pText))
-                        classText = str(info).split("<div>")
+        courses = line.split('|')
+        #print(courses)
+        
+        for course in courses:
+            
+            if course in found:
+               print("Course already has been parsed: {}".format(course))
+               continue
 
-                        newDataRow.append(deWhiteSpaceElement(pText))
+            found.add(course)
+            course = course.split(' ')
+            
+            subject = course[0]
+            cNum = course[1]
+            
+            url = "http://www.cdm.depaul.edu/academics/pages/courseinfo.aspx?Subject=" + \
+                subject + "&CatalogNbr=" + cNum
+            
+            urlOpen = urllib.request.urlopen(url)
+            soup = BeautifulSoup(urlOpen, 'html.parser')
 
-                        for element in classText:
-                            if "Section" in element:
-                                cleanElement = deWhiteSpaceElement(deDivElement(element))
-                                newDataRow.append(cleanElement)
-                                #print(cleanElement)
+            schedule = soup.findAll("div", "schedule")
 
-                            if "Class number" in element:
-                                cleanElement = deWhiteSpaceElement(deDivElement(element))
-                                newDataRow.append(cleanElement)
-                                #print(cleanElement)
+            # schedule is a Result.Set
+            # sched is an element.Tag
+            # print(type(schedule))
+            for sched in schedule:
+                season = sched.find("p", "CTIPageSectionHeader")
+                thisSeason = season.getText()
+                # append season
+                
+                
+                classInfo = sched.findAll("div", "classInfo")
+                #print(type(classInfo))
+                for isClass in classInfo:
+                    aClass = isClass.findAll("div")
 
-                            if "Meeting time" in element:
-                                cleanElement = deWhiteSpaceElement(deDivElement(element))
-                                newDataRow.append(cleanElement)
-                                #print(cleanElement)
+                    # make row. append
+                    newCourseRow = []
+                    newCourseRow.append(thisSeason)
+                
+                    for e in aClass:
+                        cleanE = newCourseRow.append(deWhiteSpaceElement(deDivElement(str(e))))
+                        # print(type(e))
+                        if "Section" in str(e):
+                            newCourseRow.append(cleanE)
+                        if "Class number" in str(e):
+                            newCourseRow.append(cleanE)
+                            
+                        if "Meeting time" in str(e):
+                            newCourseRow.append(cleanE)
 
-                            if "Location" in element:
-                                cleanElement = deWhiteSpaceElement(deDivElement(element))
-                                newDataRow.append(cleanElement)
-                                #print(cleanElement)
+                        if "Location" in str(e):
+                            newCourseRow.append(cleanE)
 
-                        #this would be instructor when its found
-                        #TODO - Instructor
+                        if "Instructor" in str(e):
+                            # get professor + links - these sometimes are not present
+                            # "" if not present
+                            try:
+                                profName = deWhiteSpaceElement(str(e.find("a").getText()))
+                            except AttributeError as err:
+                                #print("professor name not found: {}".format(err))
+                                profName = ""
+                            #print(profName)
+                            newCourseRow.append(profName)
 
-                        #then we append that row to the outer list
-                        newDataRow.append("")
-                        parsedData.append(newDataRow)
-                i = i + 2
-        ##########################
-        ###############
-        # do data parse
-        # 1 entry == 1 array
-        # for each array, form it as a row
-        # for the below line:
-        # parsedData.append(row)
-        ###############
-        return parsedData
+                            try:
+                                profUrl = depaul + ampersandCleaner(e.find("a").get("href"))
+                            except AttributeError as err:
+                                #print("professor name not found: {}".format(err))
+                                profUrl = ""
+                                
+                            #print(profUrl)
+                            newCourseRow.append(profUrl)
+                            
+                            try:
+                                syllabusUrl = depaul + deWhiteSpaceElement(ampersandCleaner(e.find("span").find("a").get("href")))
+                            except AttributeError as err:
+                                #print("professor name not found: {}".format(err))
+                                syllabusUrl = ""
+                            
+                            #print(syllabusUrl)
+                            newCourseRow.append(syllabusUrl)
+            
+                        #print(deWhiteSpaceElement(deDivElement(str(e))))
+                    
+                    # append class to data found
+                    parsedData.append(newCourseRow)
+        file.close()
 
-# this works
+    return parsedData
+
+
+
 
 def parsedDataToDataFrame(parsedData):
-        '''dumps all rows to data frame'''
+    '''dumps all rows to data frame'''
 
-        #print(parsedData)
-        season = []
-        section = []
-        classNumber = []
-        meetingTime = []
-        location = []
-        instructor = []
+    # print(parsedData)
+    season = []
+    section = []
+    classNumber = []
+    meetingTime = []
+    location = []
+    instructor = []
+    instructorUrl = []
+    syllabusUrl = []
 
-        for row in parsedData:
-                season.append(row[0])
-                section.append(row[1])
-                classNumber.append(row[2])
-                meetingTime.append(row[3])
-                location.append(row[4])
-                instructor.append(row[5])
-        #print("{}\n{}\n{}\n{}\n{}\n{}".format(season, section, classNumber, meetingTime, location, instructor))
-        parsedDataDataFrame = pd.DataFrame({'Season':season,'Section':section,'Class Number':classNumber,'Meeting Time':meetingTime,'Location':location,'Instructor':instructor})
-        parsedDataDataFrame.to_csv('Catalog.csv',index=False,columns=['Major','Season','Section','Class Number','Meeting Time','Location','Instructor'], engine='python')
+    for row in parsedData:
+        season.append(row[0])
+        section.append(row[1])
+        classNumber.append(row[2])
+        meetingTime.append(row[3])
+        location.append(row[4])
+        instructor.append(row[5])
+        instructorUrl.append(row[6])
+        syllabusUrl.append(row[7])
+        
+    parsedDataDataFrame = pd.DataFrame({'Season': season,
+                                        'Section': section,
+                                        'Class Number': classNumber,
+                                        'Meeting Time': meetingTime,
+                                        'Location': location,
+                                        'Instructor': instructor,
+                                        'Insutructor Url': instructorUrl,
+                                        'Syllabus Url':syllabusUrl})
+    parsedDataDataFrame.to_csv(
+        'Catalog.csv',
+        index=False,
+        columns=['Season',
+                 'Section',
+                 'Class Number',
+                 'Meeting Time',
+                 'Location',
+                 'Instructor',
+                 'Instructor Url',
+                 'Syllabus Url'],
+        engine='python')
 
 parsedDataToDataFrame(runParse())
