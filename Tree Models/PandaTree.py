9f12d9ddeb87899ba1e6912231ee300377198d90
Justin Tyme Dejesus <justin.tyme.dejesus@gmail.com>
2015-11-15 15:15:26 -0600

    general model works for 1 degree
    TODOS
    - search for electives
    - search for focus area electives
    - search again for course not found

--- Tree Models/PandaTree.py
+++ Tree Models/PandaTree.py
@@ -7,7 +7,6 @@ def OpenCSV2DF(file):
     '''opens a csv file
        returns it as a dataframe'''
     df = pd.read_csv(file)
-
     return df
 
 
@@ -16,15 +15,114 @@ def OpenCSV2DF(file):
 # casting this as a string returns it as a string
 # normally it is a dataframe object
 
-def testQueryPrint(df, myClass, mySeason):
-    ''' Test querying from class and season'''
+def randomQuery(df, myClass, mySeason):
+    ''' Test querying from class and season with .sample()'''
+    #this returns a dataframe cast to a string
+    out = str(df[(df['Class Name'] == myClass) & (df['Season'] == mySeason)])
+    #print(out)
+    # if empty
+    if df.empty:
+        return "COURSE NOT FOUND, SEE ADVISOR"
     
-    print(df[(df['Class Name'] == myClass) & (df['Season'] == mySeason)])
+    return out
 
 
 # test open
 myDF = OpenCSV2DF('Catalog.csv')
 
-testQueryPrint(myDF, 'CSC 421', 'Spring 2011-2012')
+randomQuery(myDF, 'ANI 423', 'Fall 2012-2013')
 
+# may need a dict instantiator helper function
 
+def toMap(classes):
+    '''maps an array of classnames to dictionary'''
+
+    return dict.fromkeys(classes, None)
+
+def majorAnimation(df):
+    ''' defines animation major requirements'''
+
+    # dict style is {season : {courseName: random panda query} }
+    courseTrack = {}
+
+    # Focus Area Electives
+    FAE = ['ANI 430', 'ANI 431', 'ANI 432', 'ANI 433',
+            'ANI 435', 'ANI 436', 'ANI 438', 'ANI 439',
+            'ANI 445', 'ANI 450', 'ANI 451', 'ANI 452',
+            'ANI 453', 'ANI 478', 'ANI 420', 'ANI 427',
+            'ANI 455', 'ANI 456', 'ANI 457','ANI 470', 'ANI 490']
+
+    # general elective choices
+    E = ['ANI', 'DC', 'GAM', 'GD', 'GPH', 'HCI', 'VFX']
+    
+    # dictionaries are class names to panda queries
+    # first year maps to fall winter spring
+    fallYearOne = 'Fall 2012-2013'
+    fallOne =  ['ANI 423','ANI 460','ANI 421']
+    fallOne = toMap(fallOne)
+    courseTrack[fallYearOne] = fallOne
+    # works
+    #print(fallOne)
+
+    # animation focus area elective is FAE
+    winterYearOne = 'Winter 2013-2014'
+    winterOne =  ['ANI 425','ANI 422','FAE']
+    winterOne = toMap(winterOne)
+    courseTrack[winterYearOne] = winterOne
+
+    # 466 can be 444
+    # animation focus areaelective is FAE
+    springYearOne = 'Spring 2013-2014'
+    springOne =  ['ANI 440','ANI 466','FAE']
+    springOne = toMap(springOne)
+    courseTrack[springYearOne] = springOne
+
+    # second year maps to fall winter with open electives later
+    # animation elective is E
+    fallYearTwo = 'Fall 2013-2014'
+    fallTwo =  ['ANI 540','FAE','E']
+    fallTwo = toMap(fallTwo)
+    courseTrack[fallYearTwo] = fallTwo
+
+    # works
+    #print(fallTwo)
+
+    # animation focus area elective is FAE
+    winterYearTwo = 'Winter 2014-2015'
+    winterTwo =  ['ANI 541','FAE']
+    winterTwo = toMap(winterTwo)
+    courseTrack[winterYearTwo] = winterTwo
+
+    # 466 can be 444
+    # animation focus areaelective is FAE
+    springYearTwo = 'Spring 2014-2015'
+    springTwo =  ['FAE','FAE','FAE']
+    springTwo = toMap(springTwo)
+    courseTrack[springYearTwo] = springTwo
+
+    
+
+    # query for course in season
+    for season in list(courseTrack.keys()):
+        print(season)
+        for course in list(courseTrack[season].keys()):
+
+            if course != 'E' and course != 'FAE':
+                print(course)
+                out = randomQuery(df, course, season)
+                print(out)
+                courseTrack[season][course] = out
+
+            if course == 'E':
+                # TODO query for electives
+                courseTrack[season][course] = "SEE ADVISOR"
+
+            if course == 'FAE':
+                # TODO queries for focus area electives
+                courseTrack[season][course] = "SEE ADVISOR"
+            
+
+    # works
+    print(courseTrack)
+    
+majorAnimation(myDF)
