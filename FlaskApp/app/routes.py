d2dcfc4b7662e76fa415cf46ae6843c33d33a2ba
Justin Tyme Dejesus <justin.tyme.dejesus@gmail.com>
2015-11-21 20:49:25 -0600

    button isn't passing in...

--- FlaskApp/app/routes.py
+++ FlaskApp/app/routes.py
@@ -30,17 +30,22 @@ def test(var):
 @app.route("/form", methods = ["GET", "POST"])
 def form(tree=None):
 
-    button = request.form.get('choice1', None) #this retrieves which radio button was pressed
 
     if request.method == "POST":
 
-        
+
+        button = request.form['choice1'] #this retrieves which radio button was pressed
+        print("$$$$$$$$$$$$$$$$$$$$$$")
+        print(button)
+        print("$$$$$$$$$$$$$$$$$$$$$$")
 
         if button == 'Animator': #if the button with attribute value = "A' is pressed
 
                 #what you want to do when button A is pressed
             tree = PandaTree.majorAnimator()
+            print("$$$$$$$$$$$$$$$$$$$$$$")
             print(tree)
+            print("$$$$$$$$$$$$$$$$$$$$$$")
             return render_template("form.html",tree=tree)
 
         elif button == 'B':
@@ -57,7 +62,7 @@ def form(tree=None):
             pass
 
     elif request.method == "GET":
-        return render_template("form.html", button=button)
+        return "it's a get!"
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
